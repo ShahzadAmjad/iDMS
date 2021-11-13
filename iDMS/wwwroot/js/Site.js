5d3fa2c81e8dcb2a37b43acbd766fb01ea2a2b1f
Administrator <shahzadamjad94@gmail.com>
2021-11-13 11:03:31 +0500

    update

---
+++ iDMS/wwwroot/js/Site.js
@@ -0,0 +1,22 @@
+﻿
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+$(function () {
+
+    var PlaceHolderElement = $('#PlaceHolderHere');
+
+    $('button[data-toggle="ajax-modal" ]').click(function (event) {
+
+        var url = $(this).data('url');
+
+        $.get(url).done(function (data) {
+
+            PlaceHolderElement.html(data);
+
+            PlaceHolderElement.find('.modal').modal('show');
+
+        })
+
+    })
+
+})
