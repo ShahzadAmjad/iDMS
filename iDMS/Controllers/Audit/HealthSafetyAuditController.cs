7e9d280939090452982152bf38ff26e098fa6cec
Administrator <shahzadamjad94@gmail.com>
2021-11-16 16:14:13 +0500

    update

--- iDMS/Controllers/Audit/HealthSafetyAuditController.cs
+++ iDMS/Controllers/Audit/HealthSafetyAuditController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using iDMS.Models.Audit;
+using iDMS.Models.Audit.HealthSafetyAudit;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +14,37 @@ namespace iDMS.Controllers.Audit
         {
             return View("~/Views/Audit/HealthSafetyAudit/HealthSafety.cshtml");
         }
+        [HttpGet]
         public ViewResult HealthSafetyAudit()
         {
-            return View("~/Views/Audit/HealthSafetyAudit/HealthSafety.cshtml");
+            HealthSafety healthSafety = new HealthSafety();
+            healthSafety.auditQuestionsLst = new List<AuditQuestions>() { };
+            AuditQuestions question1 = new AuditQuestions { qustionText = "Is the approved Health & Safety Plan and/or site-specific Method Statements on site?", ansawer = "Yes", comments = "NA" };
+
+
+            AuditQuestions question2 = new AuditQuestions { qustionText = "Have all operatives accepted and signed onto the RAMS?", ansawer = "No", comments = "NA" };
+            AuditQuestions question3 = new AuditQuestions { qustionText = "Is the site clean and generally tidy and contained within a secure working area?", ansawer = "Yes", comments = "NA" };
+            AuditQuestions question4 = new AuditQuestions { qustionText = "Are all the drawings available on site?", ansawer = "Yes", comments = "NA" };
+            AuditQuestions question5 = new AuditQuestions { qustionText = "Are all drawings available the latest versions?", ansawer = "Yes", comments = "NA" };
+            AuditQuestions question6 = new AuditQuestions { qustionText = "Is a permit to work system (required for all work involving live electrical installations) being used?", ansawer = "Yes", comments = "NA" };
+            healthSafety.auditQuestionsLst.Add(question1);
+            healthSafety.auditQuestionsLst.Add(question2);
+            healthSafety.auditQuestionsLst.Add(question3);
+            healthSafety.auditQuestionsLst.Add(question4);
+            healthSafety.auditQuestionsLst.Add(question5);
+            healthSafety.auditQuestionsLst.Add(question6);
+            //healthSafety.auditQuestionsLst.Add(question);
+            //healthSafety.auditQuestionsLst.Add(question1);
+
+
+            return View("~/Views/Audit/HealthSafetyAudit/HealthSafety.cshtml", healthSafety);
+        }
+
+        [HttpPost]
+        public IActionResult HealthSafetyAudit(HealthSafety healthSafety)
+        {
+
+            return View();
         }
     }
 }
--- iDMS/Models/Audit/HealthSafetyAudit/HealthSafety.cs
+++ iDMS/Models/Audit/HealthSafetyAudit/HealthSafety.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,14 +11,23 @@ namespace iDMS.Models.Audit.HealthSafetyAudit
     {
         [Key]
         public int HealthSafetyId { get; set; }
+        [DisplayName("Location of work")]
         public string locationOfWork { get; set; }
+        [DisplayName("Type of work")]
         public string typeOfWork { get; set; }
+        [DisplayName("Team Leader")]
         public string teamLeader { get; set; }
+        [DisplayName("Contractor")]
         public string contractor { get; set; }
+        [DisplayName("Project No")]
         public int projectNo { get; set; }
+        [DisplayName("Date/Time")]
         public DateTime dateTime { get; set; }
+        [DisplayName("Audit Questions")]
         public List<AuditQuestions> auditQuestionsLst { get; set; }
+        [DisplayName("Defect Found")]
         public string defectFound { get; set; }
+        [DisplayName("Remedial action taken")]
         public string remedialActionTaken { get; set; }
 
         //public string LocationOfWork { get; set; }
